74c670831bf15781f479c48d2913405640a785f2
EC2 Default User <ec2-user@ip-172-31-77-14.ec2.internal>
2020-09-16 04:02:57 +0000

    Update Commit

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
   def new
-
+    @book = Book.new
   end
 
   def create
-
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @book = Book.new
 
   end
 
@@ -21,5 +25,10 @@ class BooksController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'books#index'
+  root 'books#index'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
